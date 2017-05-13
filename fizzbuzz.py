ae7138f2e67a1b97f6f46083ea4aefd68e95d30d
Lynn Langit <lynnlangit@live.com>
2017-05-12 22:22:17 -0700

    integrate with Algorithmia API

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,8 @@
-from sklearn.preprocessing import LabelBinarizer
-from sklearn.linear_model import LogisticRegression
-from sklearn import cross_validation
-import numpy as np
 import pickle
-
-#import Algorithmia
-#client = Algorithmia.client()
+import numpy as np
+from sklearn import cross_validation
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import LabelBinarizer
 
 def fizz_buzz(i):
     if i % 15 == 0:
@@ -26,12 +23,11 @@ def build_samples(samples):
         yield [padding + samples[max(0, i - sequence_length):i], samples[i]]
         padding = padding[1:]
 
-
 def learn():
     num_samples = 15000
 
     fizz_buzz_samples = [fizz_buzz(i) for i in range(1, num_samples + 1)]
-    print(fizz_buzz_samples)
+    print fizz_buzz_samples
     samples = list(build_samples(fizz_buzz_samples))
 
     lb = LabelBinarizer()
@@ -40,18 +36,15 @@ def learn():
     X = np.array([np.array(lb.transform(x)).flatten() for x, y in samples])
     y = np.array([y for x, y in samples])
 
-    X_train, X_test, y_train, y_test = X[0:10000], X[10000:15000], y[0:10000], y[10000:15000], 
+    X_train, X_test, y_train, y_test = X[0:10000], X[10000:15000], y[0:10000], y[10000:15000],
 
-    # Learn a simple logistic regression classifier
     clf = LogisticRegression(tol=1e-6)
     clf.fit(X_train, y_train)
 
-    print(clf.score(X_test, y_test))
+    print clf.score(X_test, y_test)
 
-    # save binarize and classifier
     with open('binarizer.pkl', 'wb') as binarizer_file:
         pickle.dump(lb, binarizer_file)
-
     with open('classifier.pkl', 'wb') as classifier_file:
         pickle.dump(clf, classifier_file)
 
--- fizzbuzzRemote.py
+++ fizzbuzzRemote.py
@@ -1,14 +1,16 @@
 import numpy as np
 import pickle
-
 import Algorithmia
 
-client = Algorithmia.client()
+input = 'lynn'
+client = Algorithmia.client('simhJChv6RPBADABAvO1tXnlrQx1')
+algo = client.algo('lynnlangit/fizzbuzzML/0.1.0')
+print algo.pipe(input)
 
 sequence_length = 15
 
-binarizer_url = 'data://<yourname>/fizzbuzzML/binarizer.pkl'
-classifier_url = 'data://<yourname>/fizzbuzzML/classifier.pkl'
+binarizer_url = 'data://lynnlangit/fizzbuzzML/binarizer.pkl'
+classifier_url = 'data://lynnlangit/fizzbuzzML/classifier.pkl'
 binarizer_path = client.file(binarizer_url).getFile().name
 classifier_path = client.file(classifier_url).getFile().name
 with open(binarizer_path, 'rb') as binarizer_file:
@@ -19,18 +21,17 @@ with open(classifier_path, 'rb') as classifier_file:
 def apply(input):
     pad = ['PAD'] * sequence_length
     input = []
-    res = []
+    result = []
     for i in range(1, 101):
         X = lb.transform(pad + input)
         predicted = clf.predict([np.array(X).flatten()])[0]
         if predicted == "i":
-            res.append(str(i))
+            result.append(str(i))
         else:
-            res.append(predicted)
-        
+            result.append(predicted)
         input.append(predicted)
 
         pad = pad[1:]
         if len(input) > sequence_length:
             input = input[1:]
-    return res
+    return result
