37314b9edf6a4086c7fa295f7b71c2d5c4137b0c
Lynn Langit <lynnlangit@live.com>
2017-05-13 18:17:06 -0700

    refactor fizzbuzzRemote for readability

--- fizzbuzzRemote.py
+++ fizzbuzzRemote.py
@@ -2,29 +2,29 @@ import numpy as np
 import pickle
 import Algorithmia
 
-input = 'lynn'
-client = Algorithmia.client('simhJChv6RPBADABAvO1tXnlrQx1')
-algo = client.algo('lynnlangit/fizzbuzzML/0.1.0')
-print algo.pipe(input)
+INPUT = '0'
+CLIENT = Algorithmia.client('simhJChv6RPBADABAvO1tXnlrQx1')
+ALGO = CLIENT.algo('lynnlangit/fizzbuzzML/0.1.0')
+print ALGO.pipe(INPUT)
 
-sequence_length = 15
+SEQUENCE_LENGTH = 15
 
-binarizer_url = 'data://lynnlangit/fizzbuzzML/binarizer.pkl'
-classifier_url = 'data://lynnlangit/fizzbuzzML/classifier.pkl'
-binarizer_path = client.file(binarizer_url).getFile().name
-classifier_path = client.file(classifier_url).getFile().name
-with open(binarizer_path, 'rb') as binarizer_file:
-    lb = pickle.load(binarizer_file)
-with open(classifier_path, 'rb') as classifier_file:
-    clf = pickle.load(classifier_file)
+BINARIZER_URL = 'data://lynnlangit/fizzbuzzML/binarizer.pkl'
+CLASSIFIER_URL = 'data://lynnlangit/fizzbuzzML/classifier.pkl'
+BINARIZER_PATH = CLIENT.file(BINARIZER_URL).getFile().name
+CLASSIFIER_PATH = CLIENT.file(CLASSIFIER_URL).getFile().name
+with open(BINARIZER_PATH, 'rb') as binarizer_file:
+    LABEL = pickle.load(binarizer_file)
+with open(CLASSIFIER_PATH, 'rb') as classifier_file:
+    CLASSIFIER = pickle.load(classifier_file)
 
 def apply(input):
-    pad = ['PAD'] * sequence_length
+    pad = ['PAD'] * SEQUENCE_LENGTH
     input = []
     result = []
     for i in range(1, 101):
-        X = lb.transform(pad + input)
-        predicted = clf.predict([np.array(X).flatten()])[0]
+        val = LABEL.transform(pad + input)
+        predicted = CLASSIFIER.predict([np.array(val).flatten()])[0]
         if predicted == "i":
             result.append(str(i))
         else:
@@ -32,6 +32,6 @@ def apply(input):
         input.append(predicted)
 
         pad = pad[1:]
-        if len(input) > sequence_length:
+        if len(input) > SEQUENCE_LENGTH:
             input = input[1:]
     return result
